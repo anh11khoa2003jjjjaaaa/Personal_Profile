aa8d88a3d5c82c7224d0ffa1179bdb68f52292b2
anh11khoa2003jjjjaaaa <huynhanhkhoa30042019@gmail.com>
2025-10-03 11:21:01 +0700

    Commit deploy vercel again1

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    minify: 'esbuild', // hoặc 'terser'
-    cssMinify: true    // ép Vite minify CSS
-  },
+  
   plugins: [react()],
 })
